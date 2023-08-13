0e7929bb0ae896cc17c93f9027376e9ad5f5b834
huyhuanhg <huyhuanhg@gmail.com>
2023-08-13 23:37:41 +0700

    init be

---
+++ backend/src/app.module.ts
@@ -0,0 +1,8 @@
+import { Module } from '@nestjs/common';
+import { HomeModule } from './home/home.module';
+import { UserModule } from './user/user.module';
+
+@Module({
+  imports: [HomeModule, UserModule],
+})
+export class AppModule {}
